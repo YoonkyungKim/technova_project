159e299fdc3bd6edbcb4628fec36fd869ae3a504
YoonkyungKim <yoonkyungkim.nlp@gmail.com>
2021-08-29 03:40:09 -0400

    Update Login Page

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -1,70 +1,83 @@
 import React, { Component, useState } from "react";
-import { View, Text, TextInput, Button } from "react-native";
-// import { Input } from "../components/Input";
-import { Card } from "react-native-elements";
-// import firebase from "firebase";
+import { ScrollView } from "react-native";
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, ImageBackground } from "react-native";
+import commonStyles from "../style/commonStyles";
 
 const LoginPage = ({ navigation }) => {
-  const [state, setState] = useState({ email: "", password: "", error: "" });
-
-  const onButtonPress = (props) => {
-    const { email, password } = state;
-
-    setState({ ...setState, error: "" });
-    firebase
-      .auth()
-      .signInWithEmailAndPassword(email, password)
-      .then((data) => {
-        console.log("successful login!! ", data);
-        navigation.navigate("HomePage");
-      })
-      .catch((data) => {
-        console.log("failed login ", data);
-        firebase
-          .auth()
-          .createUserWithEmailAndPassword(email, password)
-          .then((data) => {
-            console.log("created user", data);
-          })
-          .catch((err) => {
-            console.log(" not created user : ", err);
-            setState({ ...state, error: "Authentication Failed." });
-          });
-      });
-  };
   return (
-    <View>
-      <Card>
-        <Card>
-          {/* <Input
-            secureTextEntry={false}
-            label="Email"
-            placeholder="user@abc.com"
-            value={state.email}
-            onChangeText={(email) => setState({ ...state, email })}
-          /> */}
-        </Card>
-        <Card>
-          <Input
-            secureTextEntry
-            label="Password"
-            placeholder="minimum 6 characters"
-            value={state.password}
-            onChangeText={(password) => setState({ ...state, password })}
-          />
-        </Card>
-        <Text style={styles.errorTextStyle}>{state.error}</Text>
-        {/* <Button onPress={onButtonPress.bind(this)} title="Login" /> */}
-        <Button title="Login" />
-      </Card>
-    </View>
+    <ScrollView>
+      <View style={commonStyles.view}>
+        <Text style={styles.title}>Conquer</Text>
+        <View
+          style={commonStyles.listDivider}
+        />
+        <TouchableOpacity
+            style={styles.task}
+        >
+            <View style={{flex: 1}}>
+                <ImageBackground source={require("../assets/login.png")} resizeMode='cover' style={{ flex: 1, justifyContent: "center"}}/>
+            </View>
+        </TouchableOpacity>
+
+        <View style={{margin: 10}}>
+          <View style={styles.container}>
+            <Text style={styles.label}>Email</Text>
+            <TextInput 
+              style={styles.input}
+              placeholder="Enter your email"
+            />
+          </View>
+
+          <View style={styles.container}>
+            <Text style={styles.label}>Password</Text>
+            <TextInput 
+              style={styles.input}
+              placeholder="Enter your password"
+            />
+          </View>
+
+          <TouchableOpacity style={commonStyles.button} onPress={() => navigation.navigate("HomePage")}>
+              <Text style={commonStyles.btnText}>Login</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </ScrollView>
   );
 };
-const styles = {
-  errorTextStyle: {
-    fontSize: 20,
-    color: "red",
-    alignItems: "center",
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 40,
+    fontWeight: "bold",
+    fontStyle: "italic",
+    textAlign: "center",
+    marginVertical: 10
   },
-};
+  container: {
+    height: 60,
+    alignItems: "left",
+    margin: 15
+  },
+  task: { 
+    height: 300,
+    alignItems: "left",
+    paddingTop: 5,
+    borderRadius: 10, 
+    margin: 10,
+    textAlign: "left"
+    // marginBottom: 2.5
+  },
+  label: {
+    paddingLeft: 5,
+  },
+  input: {
+    color: "#000",
+    paddingRight: 5,
+    paddingLeft: 5,
+    fontSize: 17,
+    lineHeight: 23,
+    flex: 2,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: "#464646",
+  },
+});
 export default LoginPage;
